40fc43529aea0f41c9acbed7f2f2ae7855160c2d
washes5 <angiewashburn1997@gmail.com>
2017-03-12 19:01:33 -0400

    mod-4-indexview

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestaurantRater.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,13 @@ namespace RestaurantRater.Controllers
 {
     public class RestaurantController : Controller
     {
-        // GET: Restaurant
+        //GET:  Restaurant
+        private RestaurantDBContext db = new RestaurantDBContext();
+        
+        // GET: Restaurants
         public ActionResult Index()
         {
-            return View();
+            return View(db.Restaurants.ToList());
         }
     }
 }
