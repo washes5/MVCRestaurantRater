abf229357f0c722b87ac70281bea02ab53fe48bd
washes5 <angiewashburn1997@gmail.com>
2017-03-12 20:00:51 -0400

    mod-6-delete

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -26,7 +26,7 @@ namespace RestaurantRater.Controllers
         //POST:  Restaurant/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "RestaurantID, Name")] Restaurant restaurant)
+        public ActionResult Create([Bind(Include = "RestaurantID, Name, Address, Rating")] Restaurant restaurant)
         {
             if (ModelState.IsValid)
             {
@@ -36,6 +36,41 @@ namespace RestaurantRater.Controllers
             }
 
             return View(restaurant);
+        }
+
+        //GET:  Restaurant/Delete
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+
+        //POST:  Restaurants/Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Restaurant restaurant = db.Restaurants.Find(id);
+            db.Restaurants.Remove(restaurant);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
